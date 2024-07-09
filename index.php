a1c21368bc5ca174c8685ade26773152dbb89142
Murad Khalidov <murad@ekexat.com>
2024-07-09 09:23:11 +0000

    index.php edited online with Bitbucket

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-header("Location: https://hookahtree.ru/catalog_retail_2024_02.pdf");
-die();
+// Specify the PDF file path
+$pdfFile = 'https://hookahtree.ru/catalog_retail_2024_02.pdf';
+
+// Set headers to display the PDF in the browser
+header('Content-Type: application/pdf');
+header('Content-Disposition: inline; filename="catalog_retail_2024_02.pdf"');
+
+// Read and output the PDF file
+readfile($pdfFile);
+?>

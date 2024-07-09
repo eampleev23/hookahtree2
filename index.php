539f44fbe91f5489381d97c5fa66e43661a01582
Murad Khalidov <murad@ekexat.com>
2024-07-09 09:37:38 +0000

    index.php edited online with Bitbucket

--- index.php
+++ index.php
@@ -1,26 +1,16 @@
 <?php
-// URL of the PDF file
-$pdfUrl = 'https://hookahtree.ru/catalog_retail_2024_02.pdf';
+// Path to the local PDF file
+$pdfFile = 'catalog_retail_2024_02.pdf';
 
-// Initialize a cURL session
-$ch = curl_init();
-
-// Set the URL
-curl_setopt($ch, CURLOPT_URL, $pdfUrl);
-
-// Return the transfer as a string
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-// Fetch the PDF content
-$pdfContent = curl_exec($ch);
-
-// Close the cURL session
-curl_close($ch);
-
-// Set headers to display the PDF in the browser
-header('Content-Type: application/pdf');
-header('Content-Disposition: inline; filename="catalog_retail_2024_02.pdf"');
-
-// Output the PDF content
-echo $pdfContent;
+// Check if the file exists
+if (file_exists($pdfFile)) {
+    // Set headers to display the PDF in the browser
+    header('Content-Type: application/pdf');
+    header('Content-Disposition: inline; filename="catalog_retail_2024_02.pdf"');
+    
+    // Read the file and output its contents
+    readfile($pdfFile);
+} else {
+    echo "The file does not exist.";
+}
 ?>

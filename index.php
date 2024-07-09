8abfc1f7edcd40e1d3499fa402d35360b69f29a0
Murad Khalidov <murad@ekexat.com>
2024-07-09 09:26:54 +0000

    index.php edited online with Bitbucket

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-// Specify the PDF file path
-$pdfFile = 'https://hookahtree.ru/catalog_retail_2024_02.pdf';
+// URL of the PDF file
+$pdfUrl = 'https://hookahtree.ru/catalog_retail_2024_02.pdf';
 
 // Set headers to display the PDF in the browser
 header('Content-Type: application/pdf');
 header('Content-Disposition: inline; filename="catalog_retail_2024_02.pdf"');
 
-// Read and output the PDF file
-readfile($pdfFile);
+// Use file_get_contents or curl to fetch the PDF content
+$pdfContent = file_get_contents($pdfUrl);
+
+// Output the PDF content
+echo $pdfContent;
 ?>

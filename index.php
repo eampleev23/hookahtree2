451097cceb1d8942d84865830fda6251059deee0
Murad Khalidov <murad@ekexat.com>
2024-07-09 09:34:05 +0000

    index.php edited online with Bitbucket

--- index.php
+++ index.php
@@ -2,12 +2,24 @@
 // URL of the PDF file
 $pdfUrl = 'https://hookahtree.ru/catalog_retail_2024_02.pdf';
 
+// Initialize a cURL session
+$ch = curl_init();
+
+// Set the URL
+curl_setopt($ch, CURLOPT_URL, $pdfUrl);
+
+// Return the transfer as a string
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Fetch the PDF content
+$pdfContent = curl_exec($ch);
+
+// Close the cURL session
+curl_close($ch);
+
 // Set headers to display the PDF in the browser
 header('Content-Type: application/pdf');
 header('Content-Disposition: inline; filename="catalog_retail_2024_02.pdf"');
-
-// Use file_get_contents or curl to fetch the PDF content
-$pdfContent = file_get_contents($pdfUrl);
 
 // Output the PDF content
 echo $pdfContent;
